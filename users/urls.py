29423581b9e2483d2228cecaa514f3aa05eedab8
Pranava S <pranavankl@gmail.com>
2024-02-02 01:05:18 +0530

    login view api added

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path,include
 from django.conf.urls.static import static
-from .views import RegisterView
+from .views import RegisterView,LoginView
 
 urlpatterns = [
     path('register/',RegisterView.as_view()),
+    path('login/',LoginView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 
 class RegisterView(APIView): 
@@ -11,3 +12,21 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+class LoginView(APIView): 
+    def post(self,request):
+        email = request.data["email"]
+        password = request.data["password"]
+
+        user = User.objects.filter(email=email).first()
+        print(user)
+
+        if user is None:
+            return Response({'detail':'user not found'})
+        
+        if not user.check_password(password):
+            return Response({'detail':'Incorrect password'})
+        
+        return Response({
+            'message':'Success!!!'
+        })
